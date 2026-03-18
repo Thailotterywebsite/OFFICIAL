eba8829aee4fcd91090db00ea9e176af8902b87a
Thailotterywebsite <tuhinali7453@gmail.com>
2026-03-18 03:14:07 -0700

    update website

--- assets/js/gate.js
+++ assets/js/gate.js
@@ -2,7 +2,7 @@ function checkCode() {
     var code = document.getElementById("accessCode").value.trim();
 
     // Multiple valid codes
-    var validCodes = ["VIP123", "THAI2026", "AGENT001"];
+    var validCodes = ["MVP007", "THAII26", "PER001"];
 
     if (validCodes.includes(code)) {
         localStorage.setItem("access_granted", "yes");
